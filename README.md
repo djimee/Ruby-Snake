72853c1260451c44061e575c915c815479f8d92e
Dipraj Jimee <jimeed@outlook.com>
2021-01-02 19:21:51 +0000

    updated readme + added new mechanics

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Ruby-Snake
+
+# Useful links:
+Ruby 2D documentation - https://www.ruby2d.com/
+Attribute accessors - https://www.rubyguides.com/2018/11/attr_accessor/https://www.rubyguides.com/2018/11/attr_accessor/
--- snake.rb
+++ snake.rb
@@ -1,14 +1,16 @@
 require 'ruby2d'
 
-set fps_cap: 1
+set fps_cap: 20
 set background: 'black'
 
-# no. of pixels 
-GRID_SIZE = 20
-# width = 640 / 20 = 32
-# length = 480 / 20 = 24
+GRID_SIZE = 20 # no. of pixels 
+WIDTH = Window.width / 20 # width = 640 / 20 = 32
+HEIGHT = Window.height / 20 # length = 480 / 20 = 24
 
 class Snake
+    # attribute writer that allows changing of direction value
+    attr_writer :direction
+
     def initialize
         @positions = [[2,0]] # 2D array as snake grows
         @direction = 'right'
@@ -18,10 +20,14 @@ class Snake
         @positions.each do |position|
             Square.new(
                 x: position[0] * GRID_SIZE, y: position[1] * GRID_SIZE,
-                size: GRID_SIZE - 1,
+                size: GRID_SIZE - 1, # -1 to get gaps between squares
                 color: 'white'
             )
         end    
+    end
+    
+    def head
+        @positions.last
     end
 
     def move
@@ -41,8 +47,13 @@ class Snake
         end
     end
 
-    def head
-        @positions.last
+    def direction?(new_direction)
+        case @direction
+        when direction = 'up' then new_direction != 'down'
+        when direction = 'down' then new_direction != 'up'
+        when direction = 'left' then new_direction != 'right'
+        when direction = 'right' then new_direction != 'left'
+        end
     end
 end
 
@@ -52,6 +63,14 @@ update do
     clear
     snake.move
     snake.draw
+end
+
+on :key_down do |event|
+    if ['up', 'down', 'left', 'right'].include?(event.key)
+        if snake.direction?(event.key)
+            snake.direction = event.key
+        end
+    end
 end
 
 show
